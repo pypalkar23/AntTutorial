6fe5e5c265361ecdb3322029811946c8312847f1
pypalkar23 <mandar.palkar94@gmail.com>
2020-05-10 23:11:05 +0530

    first commit

---
+++ General Practice/HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public void displayMessage(){
+        System.out.println("Hello World");
+    }
+}
---
+++ General Practice/Test.java
@@ -0,0 +1,6 @@
+public class Test{
+    public static void main(String[] args) {
+        HelloWorld h = new HelloWorld();
+        h.displayMessage();
+    }
+}
